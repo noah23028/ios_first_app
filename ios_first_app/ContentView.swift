6e0d27fa6e151430acc1211a4bcb867820927368
noah23028 <145073501+noah23028@users.noreply.github.com>
2024-02-01 19:09:26 -0600

    Initial Commit

---
+++ ios_first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_first_app
+//
+//  Created by Morris, Noah L (morri095) on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_first_app/ios_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_first_appApp.swift
+//  ios_first_app
+//
+//  Created by Morris, Noah L (morri095) on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
